e4a38cfce6139ef638a6fe37d8538e843c6cb09b
k24dizzle <zhaok24@uw.edu>
2016-10-15 23:14:24 -0700

    AHHHHHHH

--- server.js
+++ server.js
@@ -1,8 +1,10 @@
 var express = require('express');
 var app = express();
+var path = require('path');
 
+app.use(express.static(__dirname));
 app.get('/', function (req, res) {
-  res.send('trash');
+  res.sendFile(__dirname + '/index.html');
 });
 
 app.listen(1111, function () {

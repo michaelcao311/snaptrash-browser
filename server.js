ea469f9d75064fdd82dcb803da068456a7ad616c
michaelcao311 <caom1997@uw.edu>
2016-10-16 02:53:16 -0700

    djkfls

--- server.js
+++ server.js
@@ -3,48 +3,33 @@ var app = express();
 var util = require('util');
 var path = require('path');
 var keys = require('./keys');
-var request = require('request');
+var request = require('co-request');
 var http = require('http');
 var fs = require('fs');
 var bodyparser = require('body-parser');
 var querystring = require('query-string');
 
-var postData = querystring.stringify({
-  'grant_type': 'client_credentials',
-  'client_id': keys.clientId,
-  'client_secret': keys.clientSecret
-});
+function *postToClarifai() {
 
-var options = {
-  hostname: 'https://api.clarifai.com',
-  port: 80,
-  path: '/v1/token/',
-  method: 'POST',
-  headers: {
-    'Content-Type': 'application/x-www-form-urlencoded',
-    'Content-Length': Buffer.byteLength(postData)
+  var payload = {
+    client_id: keys.clientId,
+    client_secret: keys.clientSecret,
+    grant_type: client_credentials
   }
-};
 
-var req = http.request(options, (res) => {
-  console.log(`STATUS: ${res.statusCode}`);
-  console.log(`HEADERS: ${JSON.stringify(res.headers)}`);
-  res.setEncoding('utf8');
-  res.on('data', (chunk) => {
-    console.log(`BODY: ${chunk}`);
-  });
-  res.on('end', () => {
-    console.log('No more data in response.');
-  });
-});
+  var result = yield* request(
+    uri: 'https://api.clarifai.com/v1/token/',
+    method: 'POST',
+    form: payload
+  );
 
-req.on('error', (e) => {
-  console.log(`problem with request: ${e.message}`);
-});
+  var response = result.body;
+  var parsedResponse = JSON.parse(response);
+  console.log(parsedResponse);
 
-// write data to request body
-req.write(postData);
-req.end();
+}
+
+postToClarifai();
 
 app.use(bodyparser.json());
 app.use(express.static(__dirname));

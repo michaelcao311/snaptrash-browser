7dd7ad215df0976fae3017cf1e099408f83b95ef
michaelcao311 <caom1997@uw.edu>
2016-10-16 04:10:08 -0700

    bdslfhl"

--- index.js
+++ index.js
@@ -1,12 +1,13 @@
 var Clarifai = require('clarifai');
-var keys = require('./keys.js');
+var clientId = 'VMsW98gyAwRP0sQRU8ZP42X07i-m_f0rbjTiyzVj';
+var clientSecret = 'WjgjycdmOoDW4C_IzL6PCB0Ukn6y0jtVov-xR6CY';
 
 var app = new Clarifai.App(
   keys.clientId,
   keys.clientSecret
 );
 
-app.models.predict(Clarifai.GENERAL_MODEL, {base64: "G7p3m95uAl..."}).then(
+app.models.predict(Clarifai.GENERAL_MODEL, "https://samples.clarifai.com/metro-north.jpg").then(
   function(response) {
     // do something with response
   },
